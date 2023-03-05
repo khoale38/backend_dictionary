a5c2f5ebb931f6538848f06ac83d3149a48f29e4
Mikaaass <kevinle38@gmail.com>
2023-03-05 12:03:19 +0700

    first commit

---
+++ src/test/java/com/example/backenddictionnary/backend_dictionary/BackendDictionaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backenddictionnary.backend_dictionary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDictionaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
